f1cc1dd3645ab86cc398f29c41b8f9a2b5f2a070
Preston <preston@impactmarketing.com>
2026-04-14 15:36:29 -0600

    Simplify next.config

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,8 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  output: 'standalone',
   images: {
     remotePatterns: [],
-  },
-  experimental: {
-    serverActions: {
-      bodySizeLimit: '2mb',
-    },
   },
 };
 
